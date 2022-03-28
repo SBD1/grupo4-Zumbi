e2b4d2fe020ab922c9ee89c542973ea5d8f02cda
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2022-03-28 20:27:50 -0300

    Adicionando zumbis DML
    Co-authored-by: IgorVeludo2 <velvet.guimaraes@gmail.com>
    Co-authored-by: eddie-gomes <edvan.bgjunior@gmail.com>

--- sql/dml.sql
+++ sql/dml.sql
@@ -7,6 +7,36 @@ INSERT INTO player (vida, dano, velocidade, nome, exp_acumulado, dinheiro) VALUE
 (100, 20, 10, 'Igor', 20, 50),
 (100, 10, 5, 'Erico', 5, 120);
 
+-- INSERT: zumbi
+INSERT INTO zumbi (tipoespecializacao) VALUES
+('corredores'),
+('estaladores'),
+('baiacu'),
+('gosmento');
+
+-- INSERT: zumbi
+INSERT INTO zumbi (tipoespecializacao) VALUES
+('corredor'),
+('estalador'),
+('baiacu'),
+('gosmento');
+
+-- INSERT: corredores
+INSERT INTO corredores (id_zumbi, vida, dano, velocidade, arranque, experiencia) VALUES
+(1, 60, 5, 10, 3, 50);
+
+-- INSERT: estaladores
+INSERT INTO estaladores (id_zumbi, vida, dano, velocidade, imparabilidade, experiencia) VALUES
+(2, 100, 20, 5, true, 70);
+
+-- INSERT: baiacu
+INSERT INTO baiacu (id_zumbi, vida, dano, velocidade, resistencia, experiencia) VALUES
+(3, 180, 15, 5, 20, 150);
+
+-- INSERT: gosmento
+INSERT INTO gosmento (id_zumbi, vida, dano, velocidade, tam_gosma, distancia_tiro , experiencia) VALUES
+(4, 80, 10, 5, 2, 2, 100);
+
 -- INSERT: itens
 INSERT INTO item (tipo_especializacao) VALUES
 ('arma_branca'),
@@ -17,6 +47,7 @@ INSERT INTO item (tipo_especializacao) VALUES
 ('adrenalina');
 
 -- INSERT: armas brancas
+-- Revisar a coluna: tipo
 INSERT INTO arma_branca (id_item, nome, preco, dano, tipo, comprimento) VALUES
 (1, 'machado', 75, 35, 'corpo a corpo', 1),
 (1, 'espada', 100, 55, 'corpo a corpo', 1),
@@ -26,6 +57,7 @@ INSERT INTO arma_branca (id_item, nome, preco, dano, tipo, comprimento) VALUES
 
 
 -- INSERT: armas de fogo
+-- Revisar as colunas: tipo, velocidade_recarga
 -- Armas inspiradas no jogos Valorant e Resident Evil 4
 INSERT INTO arma_fogo (id_item, nome, preco, dano, tipo, distancia, armazenamento, velocidade_recarga, abertura_impacto) VALUES
 (2, 'classic', 65, 20, 'pistola', 4, 6, 1, 1),
@@ -34,8 +66,6 @@ INSERT INTO arma_fogo (id_item, nome, preco, dano, tipo, distancia, armazenament
 (2, 'operator', 150, 50, 'rifle de precisão', 8, 5, 2, 1),
 (2, 'bucky', 100, 80, 'escopeta', 2, 2, 1, 2);
 
-
--- INSERT: munição
 INSERT INTO municao (id_item, nome, preco, quantidade, tipo_arma, id_arma_fogo ) VALUES
 (3, 'municao pistola', 10, 14, 'pistola',1),
 (3, 'municao pistola', 10, 14, 'pistola',2),
@@ -43,14 +73,10 @@ INSERT INTO municao (id_item, nome, preco, quantidade, tipo_arma, id_arma_fogo )
 (3, 'municao rifle de precisão', 20, 5, 'rifle de precisão',4),
 (3, 'municao escopeta', 15, 8, 'escopeta',5);
 
--- INSERT: armadura
 INSERT INTO armadura (id_item, nome, preco, tipo, protecao) VALUES
 (4, 'roupas comuns', '10', 'armadura', 1),
 (4, 'colete a prova de balas', '50', 'armadura', 10);
 
-
--- INSERT: comida
--- Comidas retiradas de regiões brasileiras
 INSERT INTO comida (id_item, nome, preco, tipo, utilidade, quantidade_vida) VALUES
 (5, 'barra de cereal', 5, 'comida', 'barrinha seca trio de chocolate com coco', 5),
 (5, 'tapioca com carne de sol e queijo coalho', 15, 'tapioca delicia na manteiga de garrafa com carne de sol e queijo coalho', 'restaurar vida', 30),
@@ -58,7 +84,6 @@ INSERT INTO comida (id_item, nome, preco, tipo, utilidade, quantidade_vida) VALU
 (5, 'tacaca', 30, 'comida', 'para restaurar sua vida e deixar sua boca dormente com essa delicia', 70),
 (5, 'acaraje', 40, 'comida', 'o calor desse brasil brasileiro para matar zumbi de tanta pimenta', 100);
 
--- INSERT: adrenalina
 INSERT INTO adrenalina (id_item, nome, preco, tipo, utilidade, vida_adicional, velocidade_adicional, tempo_uso) VALUES
 (6, 'coquinha gelada', 10, 'energetico', 'aumentar vida maxima', 20, 0, 30),
 (6, 'redbull', 10, 'energetico', 'aumenta velocidade', 0, 2, 30),
