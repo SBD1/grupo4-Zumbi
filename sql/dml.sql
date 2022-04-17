80f5449e799fb3f00013dd7ebcdf2c5c2fef2c19
Guilherme Deusdará <guibanci@gmail.com>
2022-04-16 22:54:19 -0300

    Adicionando script de select dos items de uma bolsa
    Co-authored-by: IgorVeludo2 <velvet.guimaraes@gmail.com>

--- sql/dml.sql
+++ sql/dml.sql
@@ -24,6 +24,43 @@ INSERT INTO quadrado (moedas, zona) VALUES
 (1,1),
 (1,1);
 
+--INSERT: referenciando quadrado
+UPDATE quadrado 
+SET lado_norte=null ,lado_sul=4 ,lado_leste=2 ,lado_oeste=null
+WHERE id = 1;
+
+UPDATE quadrado 
+SET lado_sul=5 ,lado_leste=3 ,lado_oeste=1
+WHERE id = 2;
+
+UPDATE quadrado 
+SET lado_sul=6 ,lado_oeste=2 
+WHERE id = 3;
+
+UPDATE quadrado 
+SET lado_norte=1 ,lado_sul=7 ,lado_leste=5
+WHERE id = 4;
+
+UPDATE quadrado 
+SET lado_norte=2 ,lado_sul=8 ,lado_leste=6 ,lado_oeste=4
+WHERE id = 5;
+
+UPDATE quadrado 
+SET lado_norte=3 ,lado_sul=9 ,lado_oeste=5
+WHERE id = 6;
+
+UPDATE quadrado 
+SET lado_norte=4 ,lado_leste=8
+WHERE id = 7;
+
+UPDATE quadrado 
+SET lado_norte=5 ,lado_leste=9 ,lado_oeste=7
+WHERE id = 8;
+
+UPDATE quadrado 
+SET lado_norte=6 ,lado_oeste=8
+WHERE id = 9;
+
 
 -- INSERT: player
 INSERT INTO player (vida, dano, velocidade, nome, exp_acumulado, dinheiro) VALUES 
@@ -87,44 +124,6 @@ INSERT INTO comida (id, nome, preco, tipo, utilidade, quantidade_vida) VALUES
 INSERT INTO adrenalina (id, nome, preco, tipo, utilidade, vida_adicional, velocidade_adicional, tempo_uso) VALUES
 (6, 'coquinha gelada', 10, 'energetico', 'aumentar vida maxima', 20, 0, 30);
 
-
---INSERT: referenciando quadrado
-UPDATE quadrado 
-SET lado_norte=null ,lado_sul=4 ,lado_leste=2 ,lado_oeste=null
-WHERE id = 1;
-
-UPDATE quadrado 
-SET lado_sul=5 ,lado_leste=3 ,lado_oeste=1
-WHERE id = 2;
-
-UPDATE quadrado 
-SET lado_sul=6 ,lado_oeste=2 
-WHERE id = 3;
-
-UPDATE quadrado 
-SET lado_norte=1 ,lado_sul=7 ,lado_leste=5
-WHERE id = 4;
-
-UPDATE quadrado 
-SET lado_norte=2 ,lado_sul=8 ,lado_leste=6 ,lado_oeste=4
-WHERE id = 5;
-
-UPDATE quadrado 
-SET lado_norte=3 ,lado_sul=9 ,lado_oeste=5
-WHERE id = 6;
-
-UPDATE quadrado 
-SET lado_norte=4 ,lado_leste=8
-WHERE id = 7;
-
-UPDATE quadrado 
-SET lado_norte=5 ,lado_leste=9 ,lado_oeste=7
-WHERE id = 8;
-
-UPDATE quadrado 
-SET lado_norte=6 ,lado_oeste=8
-WHERE id = 9;
-
 -- INSERT: npc
 INSERT INTO npc (tipo_especializacao) VALUES
 ('vendedor'),
@@ -144,3 +143,108 @@ INSERT INTO estoque (id_vendedor, id_item) VALUES
 (1, 3),
 (1, 4),
 (1, 5);
+
+insert into bolsa (tamanho, player) VALUES 
+(10, 1),
+(20, 2);
+
+
+INSERT INTO instancia_item (id_item, player, bolsa, quadrado) values
+(1, null, 2, null),
+(2, null, 2, null),
+(2, null, 2, null),
+(3, null, 2, null);
+
+-- selecionar items de uma bolsa
+SELECT ii.id as "id_instancia", i.*,
+
+CASE
+
+  WHEN i.tipo_especializacao = 'arma_branca' THEN ab.nome
+  WHEN i.tipo_especializacao = 'arma_fogo' THEN af.nome
+	WHEN i.tipo_especializacao = 'municao' THEN m.nome
+	WHEN i.tipo_especializacao = 'armadura' THEN ar.nome
+	WHEN i.tipo_especializacao = 'comida' THEN co.nome
+	WHEN i.tipo_especializacao = 'adrenalina' THEN ad.nome
+  ELSE NULL
+
+END as "nome",
+
+CASE
+
+  WHEN i.tipo_especializacao = 'arma_branca' THEN ab.preco
+  WHEN i.tipo_especializacao = 'arma_fogo' THEN af.preco
+	WHEN i.tipo_especializacao = 'municao' THEN m.preco
+	WHEN i.tipo_especializacao = 'armadura' THEN ar.preco
+	WHEN i.tipo_especializacao = 'comida' THEN co.preco
+	WHEN i.tipo_especializacao = 'adrenalina' THEN ad.preco
+  ELSE NULL
+
+END as "preco"
+
+
+FROM instancia_item ii
+
+LEFT JOIN item i ON ii.id_item = i.id
+
+LEFT JOIN arma_branca ab
+ON
+CASE
+
+     WHEN i.tipo_especializacao = 'arma_branca' THEN ab.id
+
+     ELSE NULL
+
+END = i.id
+
+LEFT JOIN arma_fogo af
+ON
+CASE
+
+     WHEN i.tipo_especializacao = 'arma_fogo' THEN af.id
+
+     ELSE NULL
+
+END = i.id
+
+LEFT JOIN municao m
+ON
+CASE
+
+     WHEN i.tipo_especializacao = 'municao' THEN m.id
+
+     ELSE NULL
+
+END = i.id
+
+LEFT JOIN armadura ar
+ON
+CASE
+
+     WHEN i.tipo_especializacao = 'armadura' THEN ar.id
+
+     ELSE NULL
+
+END = i.id
+
+LEFT JOIN comida co
+ON
+CASE
+
+     WHEN i.tipo_especializacao = 'arma_branca' THEN co.id
+
+     ELSE NULL
+
+END = i.id
+
+LEFT JOIN adrenalina ad
+ON
+CASE
+
+     WHEN i.tipo_especializacao = 'arma_branca' THEN ad.id
+
+     ELSE NULL
+
+END = i.id
+
+WHERE ii.bolsa = 2
