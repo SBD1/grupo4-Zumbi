effdab6664c204d21b029f96f0913537911f7ac7
Guilherme Banci <guibanci@gmail.com>
2022-04-24 18:24:12 -0300

    fazendo jogador andar de acordo com quadrados recebidos da api
    Co-authored-by: IgorVeludo2 <velvet.guimaraes@gmail.com>
    Co-authored-by: eddie-gomes <edvan.bgjunior@gmail.com>

--- zumbi-grupo4-frontend/pages/index.tsx
+++ zumbi-grupo4-frontend/pages/index.tsx
@@ -1,38 +1,67 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useState, useEffect, SetStateAction } from 'react'
 import Button from '../components/Button'
 import MoveButton from '../components/MoveButton'
 import Terminal from '../components/Terminal'
 import styles from '../styles/home.module.css'
+import api from './api/api'
 
 const Home: NextPage = () => {
   
-  const [heroPositionX, setHeroPositionX] = useState(0)
-  const [heroPositionY, setHeroPositionY] = useState(0)
+  const [heroPosition, setHeroPosition] = useState(0)
+  const [zone, setZone] = useState([])
+  const [currentPosition, setCurrentPosition] = useState({})
+
+  const getZona = async () => {
+    const response = await api.get('/quadrado/zona/1')
+    console.log(response)
+    setZone(response.data || [])
+    setHeroPosition(response.data[0].id)
+  }
+
+  const getCurrentPosition = () => {
+    zone.some(value => {
+      if (value.id === heroPosition) {
+        setCurrentPosition(value)
+        return true
+      }
+      return false
+    })
+  }
+
+  useEffect(() => {
+    getZona()
+  }, [])
+
+  useEffect(() => {
+    getCurrentPosition()
+  }, [heroPosition])
+  
 
   const positionX= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   const positionY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
   function moveRight() {
-    if (heroPositionX < 20) {
-      setHeroPositionX(heroPositionX+1)
+    if (currentPosition.lado_leste) {
+      setHeroPosition(currentPosition.lado_leste)
     }
   }
   function moveLeft() {
-    if (heroPositionX > 0) {
-      setHeroPositionX(heroPositionX-1)
+    if (currentPosition.lado_oeste) {
+      setHeroPosition(currentPosition.lado_oeste)
     }
   }
 
   function moveUp() {
-    if (heroPositionY > 0) {
-      setHeroPositionY(heroPositionY-1)
+    if (currentPosition.lado_norte) {
+      setHeroPosition(currentPosition.lado_norte)
     }
   }
 
   function moveDown() {
-    if (heroPositionY < 20) {
-      setHeroPositionY(heroPositionY+1)
+    if (currentPosition.lado_sul) {
+      setHeroPosition(currentPosition.lado_sul)
     }
   }
 
@@ -40,16 +69,13 @@ const Home: NextPage = () => {
     <div className={styles.container}>
 
       <Terminal />
-
-      {positionY.map((resx: number)=>
-        <div className={styles.containerMap} >
-          {positionX.map((resy: number) =>
-            <div className={styles.divPositionMap}>
-              {resx === heroPositionX && resy === heroPositionY? "P": null}
+        <div  className={styles.containerMap} >
+          {zone.map((res: any, index1: number) =>
+            <div key={`${index1}${res}`} className={styles.divPositionMap}>
+              {res.id === heroPosition ? "P" : null}
             </div>
           )}
         </div>
-      )}
       
       <div className={styles.buttons}>
         <MoveButton moveLeft={moveLeft} moveRight={moveRight} moveDown={moveDown} moveUp={moveUp}/>
