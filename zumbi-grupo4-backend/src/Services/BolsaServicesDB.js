9111422a67272f0d65e28ba590b9713616763685
Guilherme Banci <guibanci@gmail.com>
2022-04-27 22:55:57 -0300

    ajustando get de bolsa

--- zumbi-grupo4-backend/src/Services/BolsaServicesDB.js
+++ zumbi-grupo4-backend/src/Services/BolsaServicesDB.js
@@ -5,7 +5,7 @@ export const getItemsBolsa = async (idBolsa) => {
   
   const query = 
   `SELECT * FROM get_instancia_items_nomes_and_precos()
-   WHERE id= ${Number(idBolsa)};
+   WHERE bolsa = ${Number(idBolsa)};
   `; // Select query
   const result = await getDBConnection(query);
   return result;
